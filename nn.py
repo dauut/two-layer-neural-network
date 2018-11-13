722a041e2c311d825fa1ced1b0da306a188daacf
dauut <davutucar@gmail.com>
2018-11-12 17:13:54 -0800

    initial commit

---
+++ nn.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib as plt
+
+
+def calculate_loss(model, X, y):
+    print()
+    # todo
+
+
+def predict(model, x):
+    print()
+    # todo
+
+
+def build_model(X, y, nn_hdim, num_passes=20000, print_loss=False):
+    print()
+    # todo
+
+def build_model_691(X, y, nn_hdim, num_passes=20000, print_loss=False):
+    print()
+    # todo
